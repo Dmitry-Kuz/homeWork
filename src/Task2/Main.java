a4ae1fefe0bfda058c10dc333ebc71641b579ff9
Dmitry-kuz <KuznetsovDmitryNikolaevich@yandex.ru>
2020-10-23 03:16:17 +0300

    homeWork2

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,15 +1,23 @@
 package Task2;
 
 public class Main {
-    public static void main (String[] args) {
 
-        byte byteVal = -128;
-        short shortVal = -32768;
-        int intVal = -2147483648;
-        long longVal = -75808;
-        float floatVal = 12.21f;
-        double doubleVal = -132.123;
-        char charVal = 'a';
-        boolean booleanVal = true;
+    public static void main(String[] args) {
+
+        int i = 0;
+        int k = 0;
+        int[] arr = new int[8];
+
+        do {
+            arr[i] = k;
+            i++;
+            k = k + 3;
+
+        }while (i < arr.length);
+
+        for ( i = 0; i < arr.length; i++) {
+              System.out.print(arr[i] + "  ");
+
+        }
     }
 }
--- src/Task6/Main.java
+++ src/Task6/Main.java
@@ -4,12 +4,40 @@ public class Main {
 
     public static void main (String[] args) {
 
-        int a = -10;
-        boolean b = true;
-        if (a >= 0) {
-            System.out.println(b);
-        } else {
-            System.out.println(!b);
+        int[] arr = {2, 3, 2, 1, 2, 2, 11, 1};
+        int sum1 = 0;
+        int sum2 = 0;
+        //int j = 0;
+        int k = 0;
+        //int i =0;
+
+        for(k = 0; k < 8; k++) {
+
+            for ( int i = k; i < arr.length; i++) {
+                sum1 = sum1 + arr[i];
+                sum2 = 0;
+                for (int j = arr.length-1; j > k; j--) {
+                    sum2 = sum2 + arr[j];
+
+                }
+                break;
+            }
+            if (sum1 == sum2) {
+                System.out.println(true);
+                break;
+            }
         }
+       /* for(int i = 0, j = 8; i < j ; i++, j--){
+             sum1 = sum1 + arr[i];
+             sum2 = sum2 + arr[j-1];
+             if (sum1 == sum2){
+             System.out.println(true);
+             break;
+             }
+
+        }*/
+
+
+
     }
 }
